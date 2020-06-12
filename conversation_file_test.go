f307043216b2315d7de0baf5336e4cc3394c00ba
Slavik Shynkarenko <sl_pm@yahoo.com>
2020-06-12 22:07:08 +0200

    Fixed conversation file tests.

--- conversation_file_test.go
+++ conversation_file_test.go
@@ -1,6 +1,7 @@
 package ubahn
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,60 +13,57 @@ func Test_NewConversationFile(t *testing.T) {
 	assert.False(t, file.Empty())
 	assert.Nil(t, err)
 	assert.NotEmpty(t, file.Data)
-	assert.Equal(t, 1, file.Version)
+	assert.Equal(t, 2, file.Version)
 }
 
 func Test_NewConversationFile_InvalidFile(t *testing.T) {
-	file, err := newTestConversationFile("v1", "invalid.yml")
+	file, err := newTestConversationFile("weird", "invalid.yml")
 
 	assert.True(t, file.Empty())
 	assert.NotNil(t, err)
 }
 
 func Test_NewConversationFile_WhenFileDoesNotExist(t *testing.T) {
-	file, err := newTestConversationFile("v1", "")
+	file, err := newTestConversationFile(defaultTestConversationName, "")
 
 	assert.True(t, file.Empty())
 	assert.NotNil(t, err)
 }
 
 func Test_NewConversationFile_Version(t *testing.T) {
-	file, _ := newTestConversationFile("v1", "v1.yml")
-
-	assert.Equal(t, 1, file.Version)
-
-	file, _ = newTestConversationFile("v1", "v2.yml")
+	file, _ := newDefaultTestConversationFile()
 
 	assert.Equal(t, 2, file.Version)
 }
 
 func Test_Parse(t *testing.T) {
 	file, _ := newDefaultTestConversationFile()
-	var hash map[string]interface{}
-	err := file.Parse(&hash)
+	var config flowConfig
+
+	err := file.Parse(&config)
 
 	assert.Nil(t, err)
-
-	sequence := hash["sequence"].([]interface{})
-	assert.Equal(t, 2, len(sequence))
-	assert.Equal(t, "welcome", sequence[0].(string))
-	assert.Equal(t, "weather-report", sequence[1].(string))
+	_, hasKey := config.Outputs[0]["default-welcome"]
+	assert.True(t, hasKey)
 }
 
 func Test_FileName(t *testing.T) {
 	file, _ := newDefaultTestConversationFile()
 
-	assert.Equal(t, defaultConversationFileName, file.FileName())
+	assert.Equal(t, defaultTestConversationFileName, file.FileName())
 }
 
 func Test_FilePath(t *testing.T) {
 	file, _ := newDefaultTestConversationFile()
 
-	assert.Equal(t, "../test_data/v1/"+defaultConversationFileName, file.FilePath())
+	assert.Equal(t,
+		fmt.Sprintf("./test_data/%s/flows/%s", defaultTestConversationName, defaultTestConversationFileName),
+		file.FilePath())
 }
 
-const defaultConversationFileName = "weather.yml"
+const defaultTestConversationName = "weather"
+const defaultTestConversationFileName = "default.yml"
 
 func newDefaultTestConversationFile() (*ConversationFile, error) {
-	return newTestConversationFile("v1", defaultConversationFileName)
+	return newTestConversationFile(defaultTestConversationName, defaultTestConversationFileName)
 }
